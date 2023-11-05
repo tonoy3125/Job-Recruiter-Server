5711d6dd56edf8c7fb36435b27b5541c894ff830
tonoy3125 <shaifshajedt@gmail.com>
2023-11-06 02:57:30 +0600

    jobs by id get method added commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -34,7 +34,14 @@ async function run() {
         // Collections
         const jobCollection = client.db("jobDB").collection("job")
 
-
+        // Get jobs By id
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const cursor = jobCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
 
         // Get jobs by category_name
         app.get('/job/:category_name', async (req, res) => {
