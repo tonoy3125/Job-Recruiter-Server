069f41d9a30e791c78be2985f34397ae55b6315b
tonoy3125 <shaifshajedt@gmail.com>
2023-11-07 18:59:20 +0600

    jwt post method using and cors origin added commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
@@ -7,7 +8,10 @@ const port = process.env.PORT || 5000
 
 
 // Middleware 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5174'],
+    credentials: true
+}))
 app.use(express.json())
 
 
@@ -34,6 +38,25 @@ async function run() {
         // Collections
         const jobCollection = client.db("jobDB").collection("job")
         const bidCollection = client.db("jobDB").collection("bid")
+
+
+        // Auth Related Api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+                    // sameSite: 'none'
+                })
+                .send({ success: true })
+        })
+
+
+
 
 
 
